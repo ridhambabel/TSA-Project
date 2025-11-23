b2600054687c946eed9c32afbb2ded609fb95141
puddielaka <ridhambabel419@gmail.com>
2025-11-23 01:55:50 -0600

    Hero

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,1 +1,26 @@
-<template><div class="text-red-500">hello world</div></template>
+<template>
+  <!-- find good text color -->
+  <div class="">
+    <div class="h-[90vh] relative w-full">
+      <img src="/assets/img/TopIMG.jpg" class="overflow-y-hidden" />
+      <div
+        class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-9xl font-bold"
+      >
+        TEXT
+      </div>
+      <div class="flex justify-center">
+        <!-- bg-teal-800 or transparent -->
+        <div
+          class="px-[2vw] font-bold absolute bottom-[5vh] w-[80vw] h-[12vh] shadow-2xl bg-transparent backdrop-blur-lg rounded-full flex justify-between items-center"
+        >
+          <div>Text</div>
+          <div>Text</div>
+          <div>Text</div>
+          <div>Text</div>
+          <div>Text</div>
+          <div>Text</div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
